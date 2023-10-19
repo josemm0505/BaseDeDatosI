8e98219995204b8b5c5d7322bfddd9dd429043db
José Mejía Micolta <101059461+josemm0505@users.noreply.github.com>
2023-10-19 09:05:51 -0500

    Modificaciones de SqlSigeca.sql

--- SqlSigeca.sql
+++ SqlSigeca.sql
@@ -82,3 +82,15 @@ create table receptor(
         eliminado tinyint default 0,
         foreign key (iddocumento) references documento(iddocumento),
         foreign key (idpersona) references persona(idpersona));
+
+create view vista_sexo as select idsexo, nombre where sexo.eliminado=0;
+create view vista_estadocivil as select idestadocivil, nombre where estadocivil.eliminado=0;
+create view vista_tiposangre as select idtiposangre, nombre where tiposangre.eliminado=0;
+create view vista_repositorio as select idrepositorio, nombre where repositorio.eliminado=0;
+create view vista_directorio as select iddirectorio, nombre, idrepositorio where directorio.eliminado=0;
+create view vista_permiso as select idpermiso, nombre where permiso.eliminado=0;
+create view vista_tipodocumento as select idtipodocumento, nombre where tipodocumento.eliminado=0;
+create view vista_persona as select persona* where persona.eliminado=0;
+create view vista_documento as select documento* where documento.eliminado=0;
+create view vista_propietario as select propietario* where propietario.eliminado=0;
+create view vista_receptor as select receptor where receptor.eliminado=0;
